42f6655760503596e5f58dc7d4b6fd32e875250a
Miguel_Fr3 <rm99997@fiap.com.br>
2023-08-07 22:15:12 -0300

    Criação das cartas

---
+++ twentyone.py
@@ -0,0 +1,34 @@
+import random
+
+def cria():
+    cartas = []
+    naipes = '♦♠♥♣'
+    for n in naipes:
+        for i in range(1, 14):
+            cartas.append((i, n))
+
+    #print(cartas)
+    return cartas
+
+def compra(monte: list):
+    return monte.pop()
+
+def distribui(monte: list, qtd: int):
+    mao = []
+    while qtd > 0:
+        c = compra(monte)
+        mao.append(c)
+        qtd = qtd - 1
+    return mao
+
+def embaralha(monte: list):
+    outra_lista = []
+    while len(monte) > 0:
+        pos = random.randint(0, len(monte) - 1)
+        outra_lista.append(monte.pop(pos))
+    monte = outra_lista
+
+baralho = cria()
+c = compra(baralho)
+
+print(c)
